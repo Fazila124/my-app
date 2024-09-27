1992ee06a67633a38ad01c309fefafc0258dd59d
unknown <shahulhameednmsh@gmail.com>
2024-09-27 09:19:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Rendering from "./rendering-work/Rendering";
+function App()
+{
+  return (<><Rendering/>
+  </>)
+}
+export default App;
---
+++ src/rendering-work/Rendering.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+function Rendering()
+{
+    return(<><h1>Fazi</h1>
+    </>)
+}
+export default Rendering;
